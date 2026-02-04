a26907f88d22065d3c28fd1ebc1478c491fefa57
Juan C. Villada <jvillada@lbl.gov>
2026-02-04 14:11:34 -0800

    Release 0.7.35

--- moducomp/__init__.py
+++ moducomp/__init__.py
@@ -2,7 +2,7 @@
 moducomp: metabolic module completeness and complementarity for microbiomes.
 """
 
-__version__ = "0.7.33"
+__version__ = "0.7.35"
 __author__ = "Juan C. Villada"
 __email__ = "jvillada@lbl.gov"
 __title__ = "moducomp"
