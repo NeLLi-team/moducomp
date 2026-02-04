c7b10d61b8f85dda404e5e2d1d5fc25f43d6cc7e
Juan C. Villada <jvillada@lbl.gov>
2026-02-04 12:47:53 -0800

    Bump version to 0.7.25 in __init__.py and recipe.yaml

--- moducomp/__init__.py
+++ moducomp/__init__.py
@@ -2,7 +2,7 @@
 moducomp: metabolic module completeness and complementarity for microbiomes.
 """
 
-__version__ = "0.7.24"
+__version__ = "0.7.25"
 __author__ = "Juan C. Villada"
 __email__ = "jvillada@lbl.gov"
 __title__ = "moducomp"
